2870ba115e9817f04210af6d5d808a30c9dafacb
Giwi <giwi@free.fr>
2012-11-16 17:45:14 +0100

    0.0.2-SNAPSHOT

---
+++ src/test/java/org/giwi/camel/kafka/test/KafkaComponentTest.java
@@ -0,0 +1,30 @@
+package org.giwi.camel.kafka.test;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class KafkaComponentTest extends CamelTestSupport {
+
+	@Test
+	public void testKafka() throws Exception {
+		MockEndpoint mock = getMockEndpoint("mock:result");
+		mock.expectedMinimumMessageCount(1);
+
+		assertMockEndpointsSatisfied();
+	}
+
+	@Override
+	protected RouteBuilder createRouteBuilder() throws Exception {
+		return new RouteBuilder() {
+			@Override
+			public void configure() {
+				from("timer://foo?fixedRate=true&period=5000").setBody(constant("hello from Giwi Softwares")).to("kafka:TOPIC-TEST?zkConnect=localhost:2181");
+
+				// Recieving
+				from("kafka:TOPIC-TEST?groupId=camelTest&zkConnect=localhost:2181").log("${body}");
+			}
+		};
+	}
+}
